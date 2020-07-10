24db34dd115efef6f91fd937ff89ad47bb53ac65
jaipee <mac@Macs-Mac-mini.local>
2020-07-10 17:36:41 +0530

    initial commit

---
+++ FamacashAssesment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamacashAssesment
+//
+//  Created by jaipee on 10/07/20.
+//  Copyright © 2020 JaiPrakashYadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
